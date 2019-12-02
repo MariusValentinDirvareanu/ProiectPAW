2ce999a6e779255881896db93ee4814b25a04996
cursant <cursant@laborator.local>
2019-12-02 14:06:32 +0200

    Grayscale

--- scripturi.js
+++ scripturi.js
@@ -1,34 +1,29 @@
-function gray(){
-    var x = document.getElementById('img1').src;
-    x = grayscale(x);
+function gray() {
+    var imgObj = document.getElementById('img1');
+    imgObj.src = gray(imgObj);
 }
 
-
-function grayscale(image){
+function gray(imgObj) {
+    var canvas = document.createElement('canvas');
     var canvasContext = canvas.getContext('2d');
-    var imgW = image.width;
-    var imgH = image.height;
+
+    var imgW = imgObj.width;
+    var imgH = imgObj.height;
     canvas.width = imgW;
     canvas.height = imgH;
 
-    canvasContext.drawImage(image, 0, 0);
-
+    canvasContext.drawImage(imgObj, 0, 0);
     var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
 
-
-    for(var y = 0; y < imgPixels.height; y++){
-        for(var x = 0; x < imgPixels.width; x++){
-          var i = (y * 4) * imgPixels.width + x * 4;
-          var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-          imgPixels.data[i] = avg;
-          imgPixels.data[i + 1] = avg;
-          imgPixels.data[i + 2] = avg;
+    for (var y = 0; y < imgPixels.height; y++) {
+        for (var x = 0; x < imgPixels.width; x++) {
+            var i = (y * 4) * imgPixels.width + x * 4;
+            var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+            imgPixels.data[i] = avg;
+            imgPixels.data[i + 1] = avg;
+            imgPixels.data[i + 2] = avg;
         }
     }
-
     canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-
-    dataurl = canvas.toDataURL();
-    var newimg = "data:image/jpeg;base64," + dataurl;
-    return newimg;
+    return canvas.toDataURL();
 }
