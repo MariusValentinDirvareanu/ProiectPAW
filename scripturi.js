c4d2964c0bfa4964d280340f321d4030b2a99f26
cursant <cursant@laborator.local>
2019-12-02 15:07:32 +0200

    Auth

--- scripturi.js
+++ scripturi.js
@@ -10,3 +10,23 @@ function store() {
     localStorage.username = document.getElementById('username').value;
     localStorage.password = document.getElementById('password').value;
 }
+
+window.onload = function () {
+    if (localStorage.username != null) {
+        document.getElementById('auth').style.display = '';
+        document.getElementById('sub').style.display = 'none';
+    }
+    else {
+        document.getElementById('auth').style.display = 'none';
+        document.getElementById('sub').style.display = '';
+    }
+}
+
+function auth() {
+    if (document.getElementById('username').value == localStorage.username && document.getElementById('password').value == localStorage.password) {
+        alert('Bun venit!');
+    }
+    else{
+        alert('Username sau parola gresita!');
+    }
+}
