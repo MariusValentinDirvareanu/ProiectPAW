389d09cf482d89bb7f6501aad9afd6c9c64fae74
cursant <cursant@laborator.local>
2019-12-02 14:22:59 +0200

    Grayscale done

--- scripturi.js
+++ scripturi.js
@@ -1,29 +1,7 @@
 function gray() {
-    var imgObj = document.getElementById('img1');
-    imgObj.src = gray(imgObj);
+    document.getElementById("img1").style.filter = "grayscale(100%)";
 }
 
-function gray(imgObj) {
-    var canvas = document.createElement('canvas');
-    var canvasContext = canvas.getContext('2d');
-
-    var imgW = imgObj.width;
-    var imgH = imgObj.height;
-    canvas.width = imgW;
-    canvas.height = imgH;
-
-    canvasContext.drawImage(imgObj, 0, 0);
-    var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
-
-    for (var y = 0; y < imgPixels.height; y++) {
-        for (var x = 0; x < imgPixels.width; x++) {
-            var i = (y * 4) * imgPixels.width + x * 4;
-            var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-            imgPixels.data[i] = avg;
-            imgPixels.data[i + 1] = avg;
-            imgPixels.data[i + 2] = avg;
-        }
-    }
-    canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-    return canvas.toDataURL();
+function nogray() {
+    document.getElementById("img1").style.filter = "grayscale(0%)";
 }
