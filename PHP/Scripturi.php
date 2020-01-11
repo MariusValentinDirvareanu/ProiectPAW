77b420b528fe57e184ae1a02593796c90d3f0927
marius <dirvareanumarius@live.com>
2020-01-11 19:28:17 +0200

    Formular + conectare la MySQL

--- PHP/Scripturi.php
+++ PHP/Scripturi.php
@@ -1,3 +1,8 @@
-<?PHP
-
+<?php
+$link = mysql_connect('localhost', 'webuser', 'password');
+if (!$link) {
+    die('Esec la conectare: ' . mysql_error());
+}
+echo 'Conectat cu succes';
+mysql_close($link);
 ?>
