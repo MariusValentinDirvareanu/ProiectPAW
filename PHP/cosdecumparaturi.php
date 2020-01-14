3ecba43963e93c6e2a72b3cd94e3231e242453c8
marius <dirvareanumarius@live.com>
2020-01-14 17:57:38 +0200

    Formular + conectare baza de date(nu e finalizat)

--- PHP/cosdecumparaturi.php
+++ PHP/cosdecumparaturi.php
@@ -1,8 +1,35 @@
 <?php
-$link = mysql_connect('localhost', 'webuser', 'password');
-if (!$link) {
-    die('Esec la conectare: ' . mysql_error());
+$servername = "localhost";
+$username = "webuser";
+$password = 'YkMI$m6H7cN4';
+$dbname = "proiectdmva";
+
+$user = $_POST['user1'];
+$produs=$_POST['produs1'];
+$firma=$_POST['firma1'];
+$scrurtadescriere=$_POST['scrurtadescriere1'];
+$pretbucata= $_POST['pretbucata1'];
+$cantitate = $_POST['cantitate1'];
+
+
+
+
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-echo 'Conectat cu succes';
-mysql_close($link);
-?>
+
+$sql = "INSERT INTO stock (user, produs, firma, scrurtadescriere, pretbucata, cantitate)
+VALUES ('$user','$produs','$firma','$scrurtadescriere',$pretbucata,$cantitate)";
+
+if (mysqli_query($mysqli, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+}
+
+
+
+
+$mysqli->close();
