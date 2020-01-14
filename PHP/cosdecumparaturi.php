2da753fa9d68eb88a9d725203e104206298fd17b
marius <dirvareanumarius@live.com>
2020-01-14 19:00:50 +0200

    Modificare baza de date si fisier php

--- PHP/cosdecumparaturi.php
+++ PHP/cosdecumparaturi.php
@@ -5,31 +5,31 @@ $password = 'YkMI$m6H7cN4';
 $dbname = "proiectdmva";
 
 $user = $_POST['user1'];
+$email = $_POST['email1'];
 $produs=$_POST['produs1'];
 $firma=$_POST['firma1'];
-$scrurtadescriere=$_POST['scrurtadescriere1'];
-$pretbucata= $_POST['pretbucata1'];
 $cantitate = $_POST['cantitate1'];
-
-
-
-
+$data= date('Y-m-d H:i:s');
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$sql = "INSERT INTO stock (user, produs, firma, scrurtadescriere, pretbucata, cantitate)
-VALUES ('$user','$produs','$firma','$scrurtadescriere',$pretbucata,$cantitate)";
+$sql = "INSERT INTO comenzi (user, email, produs, firma, cantitate, dataTimp)
+VALUES ('$user', '$email', '$produs', '$firma', $cantitate, '$data')";
+
 
 if (mysqli_query($mysqli, $sql)) {
-    echo "New record created successfully";
+    $q_myQuery = "SELECT `id` FROM `comenzi` WHERE `dataTimp` = '$data'";
+    $rsmyQuery = mysqli_query($mysqli, $q_myQuery) or die(mysqli_error($mysqli));
+    $row_rsmyQuery = mysqli_fetch_assoc($rsmyQuery);
+    $img = $row_rsmyQuery['id'];
+
+    echo "Numar comanda: $img";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
 }
-
-
 
 
 $mysqli->close();
