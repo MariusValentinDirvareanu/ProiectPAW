32c211fb7786dabdc90f84a9202468ec7ddcf8f7
marius <dirvareanumarius@live.com>
2020-01-15 10:54:24 +0200

    Script mutare meniu pagina 2

--- JS/scripturi.js
+++ JS/scripturi.js
@@ -11,12 +11,11 @@ function store() {
     localStorage.password = document.getElementById('password').value;
 }
 
-window.onload = function () {
+window.onload = function() {
     if (localStorage.username != null) {
         document.getElementById('aut').style.display = '';
         document.getElementById('sub').style.display = 'none';
-    }
-    else {
+    } else {
         document.getElementById('aut').style.display = 'none';
         document.getElementById('sub').style.display = '';
     }
@@ -25,8 +24,7 @@ window.onload = function () {
 function auth() {
     if (document.getElementById('username').value == localStorage.username && document.getElementById('password').value == localStorage.password) {
         alert('Bun venit!');
-    }
-    else {
+    } else {
         alert('Username sau parola gresita!');
     }
 }
@@ -34,4 +32,16 @@ function auth() {
 function schimbareculoare() {
     var x = document.getElementById('main');
     x.style.backgroundColor = 'red';
+}
+
+function meniu() {
+    var x = document.getElementsByTagName('nav');
+    x[0].style.cssFloat = 'left';
+    x[0].style.height = '100%';
+    var y = document.getElementById('main');
+    y.style.cssFloat = 'right';
+    var z = document.getElementsByTagName('li');
+    for (let index = 0; index < z.length; index++) {
+        z[index].style.cssFloat = 'none';
+    }
 }
